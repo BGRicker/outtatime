d1cfe6442717535566bfcb4afd03b6c289621c88
Ben Ricker <BGRicker@gmail.com>
2017-01-16 00:23:07 -0500

    posts before refactor

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -11,5 +11,21 @@ describe 'navigate' do
       visit posts_path
       expect(page).to have_content(/Posts/)
     end
+
+    it 'has a form to create Posts' do
+      visit new_post_path
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'can be created from new form page' do
+      visit new_post_path
+
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "Flavortown"
+      click_on 'submit'
+
+      expect(page).to have_content(Date.today)
+      expect(page).to have_content("Flavortown")
+    end
   end
 end
